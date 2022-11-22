56816bf41a59e1ebefdc91171ebab7b7f29f97b3
mrdiobox <antti.thunder@gmail.com>
2022-11-22 12:00:46 +0300

    First commit

---
+++ app/Http/Controllers/ParseController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Goutte\Client;
+use Illuminate\Http\Request;
+use App\Models\Narva;
+use App\Models\Car;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Carbon;
+
+class ParseController extends Controller
+{
+    private $row_results = Array();
+    private $results = Array();
+
+    public function addNarvaId($arr, $narva_id) {
+        foreach ($arr as $k=>$v) {
+            $ret[$k] = $v;
+            $ret[$k]['narva_id'] = $narva_id;
+        }
+        return $ret;
+    }
+
+    public function uptoOrNum($row) {
+        if ((trim($row) == 'up to 1 hour') OR (trim($row) == 'On time')) {
+            $ret = 0;
+        }
+        else {
+            $ret = trim(preg_replace("/[^0-9]/", '', $row));
+        }
+        return $ret;
+    }
+
+    public function doParse () {
+        $client = new Client();
+        $url_table = 'https://www.eestipiir.ee/yphis/borderQueueInfo.action';
+        $url_cars_ab = 'https://www.eestipiir.ee/yphis/showCalledVehicles.action?borderCrossingQueue.id=1';
+        $url_cars_c = 'https://www.eestipiir.ee/yphis/showCalledVehicles.action?borderCrossingQueue.id=2';
+        
+        //Narva main table
+        $page = $client->request(method:'GET', uri:$url_table);
+        $narva_tbl = $page->filter('table.maintable')->first();
+
+        $narva_tbl->filter('td')->each(function($item){
+            $this->row_results['narva'][$item->attr('id')] =  $item->text();
+        });
+
+        $this->results['narva']['lql_ab'] = $this->uptoOrNum($this->row_results['narva']['lql-1']);
+        $this->results['narva']['nvl_ab'] = $this->row_results['narva']['nvl-1'];
+        $this->results['narva']['nvf_ab'] = $this->row_results['narva']['nvf-1'];
+        $this->results['narva']['frt_ab'] = Carbon::parse($this->row_results['narva']['frt-1']);
+        $this->results['narva']['lagh_ab'] = $this->uptoOrNum($this->row_results['narva']['lagh-1']);
+
+        $this->results['narva']['nvl_c'] = $this->row_results['narva']['nvl-2'];
+        $this->results['narva']['nvf_c'] = $this->row_results['narva']['nvf-2'];
+        $this->results['narva']['frt_c'] = Carbon::parse($this->row_results['narva']['frt-2']);
+        $this->results['narva']['lagh_c'] = $this->uptoOrNum($this->row_results['narva']['lagh-2']);
+        
+        $validator = Validator::make($this->results['narva'], [
+            'lql_ab' => 'integer',
+            'nvl_ab' => 'integer',
+            'nvf_ab' => 'integer',
+            'frt_ab' => 'date',
+            'lagh_ab' => 'integer',
+
+            'nvl_c' => 'integer',
+            'nvf_c' => 'integer',
+            'frt_c' => 'date',
+            'lagh_c' => 'integer',
+        ]);
+
+        if ($validator->fails()) {
+           \Log::debug('Narva parse validation error', $this->results['narva']);
+        }
+ 
+        $narva = new Narva();
+        
+        $narva->lql_ab = $this->results['narva']['lql_ab'];
+        $narva->nvl_ab = $this->results['narva']['nvl_ab'];
+        $narva->nvf_ab = $this->results['narva']['nvf_ab'];
+        $narva->frt_ab = $this->results['narva']['frt_ab'];
+        $narva->lagh_ab = $this->results['narva']['lagh_ab'];
+
+        $narva->nvl_c = $this->results['narva']['nvl_c'];
+        $narva->nvf_c = $this->results['narva']['nvf_c'];
+        $narva->frt_c = $this->results['narva']['frt_c'];
+        $narva->lagh_c = $this->results['narva']['lagh_c'];
+        $narva->save();
+        $last_id = $narva->id;
+
+        $car = new Car();
+        //Cars AB
+        $page = $client->request(method:'GET', uri:$url_cars_ab);
+        $list_tbl = $page->filter('table.maintable')->filter('td');
+        if ($list_tbl->count()) {
+            $list_tbl->filter('td')->each(function($item, $i){
+                $this->row_results['cars_ab'][$i]['number'] =  $item->text();
+                $this->row_results['cars_ab'][$i]['car_type'] =  'ab';
+            });
+            $this->results['cars_ab'] = $this->addNarvaId($this->row_results['cars_ab'], $last_id);
+            $car->insert($this->results['cars_ab']);
+        }
+        //Cars C
+        $page = $client->request(method:'GET', uri:$url_cars_c);
+        $list_tbl = $page->filter('table.maintable')->filter('td');
+        if ($list_tbl->count()) {
+            $list_tbl->each(function($item, $i){
+                $this->row_results['cars_c'][$i]['number'] = $item->text();
+                $this->row_results['cars_c'][$i]['car_type'] = 'c';
+            });
+            $this->results['cars_c'] = $this->addNarvaId($this->row_results['cars_c'], $last_id);
+            $car->insert($this->results['cars_c']);
+        }
+
+        echo 'ok!';
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use App\Http\Controllers\ParseController;
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::get('/doparse', [ ParseController::class, 'doParse' ]);
+Route::get('/clear', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('config:cache');
+    Artisan::call('view:clear');
+    Artisan::call('route:clear');
+ 
+    return "Кэш очищен.";
+});
