64ed5e3cf3735def9c162391a6d8290084ac93a6
mrdiobox <antti.thunder@gmail.com>
2022-12-27 15:09:55 +0300

    add yandex id

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <meta name="yandex-verification" content="edd5013949f75dcc" />
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
