5a3dff8b333273cf0749e803b38d933f321b5076
mrdiobox <antti.thunder@gmail.com>
2023-01-04 17:56:54 +0300

    add tg widget

--- resources/views/ee.blade.php
+++ resources/views/ee.blade.php
@@ -8,7 +8,11 @@
             <h1>Очередь Нарва - Ивангород</h1>
             <vue-ellipse-progress-b class="w-100"></vue-ellipse-progress-b>
             <h2>Список вызванных</h2>
-
+<script type="text/javascript">
+  function onTelegramAuth(user) {
+    alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+  }
+</script>
             <script type="application/javascript" async src="https://telegram.org/js/telegram-widget.js?21" data-telegram-login="ee2ru_bot" data-size="medium" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
 
             <car-numbers class="mb-10"></car-numbers>
