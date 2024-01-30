1844132eb54647d66a7924af908d45d8a0e45b97
mrdiobox <antti.thunder@gmail.com>
2024-01-30 12:48:33 +0300

    fix1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     <title>{{ $title ?? 'Заголовок'}}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js?256') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
